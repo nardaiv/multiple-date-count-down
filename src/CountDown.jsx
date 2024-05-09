9c09e82fc6b2406307980dc6a18a2229b222f3a3
nardaiv <92304461+nardaiv@users.noreply.github.com>
2024-05-09 19:42:15 +0700

    adjusting width for Menu and CountDown in md size

--- src/CountDown.jsx
+++ src/CountDown.jsx
@@ -5,7 +5,7 @@ export default function CountDown(props){
     const eventDate = new Date(selectedEvent.date);
     const remainingTime =  Math.ceil((eventDate.getTime() - props.currentDate.getTime())/(1000*60*60*24));
     return (
-        <div className='bg-grey border-2 border-darkgrey w-full md:w-9/12 lg:w-3/12 rounded-xl p-3 mt-5 text-center'>
+        <div className='bg-grey border-2 border-darkgrey w-full md:w-6/12 lg:w-3/12 rounded-xl p-3 mt-5 text-center'>
             <h2 className='font-bold text-[26px]'>{selectedEvent.title}</h2>
             <p className='text-mainblue text-[128px] font-bold'>{remainingTime}</p>
             <p className='text-faintblack font-semibold text-[32px]'>Day{remainingTime >1 ? "s": ""} Left</p>
