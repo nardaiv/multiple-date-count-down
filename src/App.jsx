9e39c284ba5b719e7157ca4371fb8cd86ad38885
nardaiv <92304461+nardaiv@users.noreply.github.com>
2024-05-10 20:26:09 +0700

    adding new state for the date instead a variable

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,30 @@ import OtherSchedule from './OtherSchedule';
 function App() {
   const [currentDate, setCurrentDate ]= React.useState(new Date());
   const [selectedMenu, setSelectedMenu] = React.useState(0);
-
+  const [processedData, setProcessedData]= React.useState([])
   function changeSelected(id){
     setSelectedMenu(id)
   }
 
-  //add status date to tell which date has already in the past
-  const processedData = Data.schedules.map( (val, i)=> {
+  React.useEffect(()=> {
+    //add status date to tell which date has already in the past
+    setProcessedData( Data.schedules.map( (val, i)=> {
 
-    const newData = val.events.map( el => {
-      const eventDate = new Date(el.date)
+      const newData = val.events.map( el => {
+        const eventDate = new Date(el.date)
 
-      //check the date by comparing with the currentDate (in ms)
-      let eventStatusInPast = eventDate.getTime() < currentDate.getTime() ? true : false;
+        //check the date by comparing with the currentDate (in ms)
+        let eventStatusInPast = eventDate.getTime() < currentDate.getTime() ? true : false;
 
-      return {
-        ...el,
-        isEventPast : eventStatusInPast 
-      }
-    }) 
+        return {
+          ...el,
+          isEventPast : eventStatusInPast 
+        }
+      }) 
 
-    return newData 
-  })
+      return newData 
+    }))
+  }, [])
 
   return (
     <div className='p-5 gap-10 flex flex-col justify-center items-center '>
